16853c9e61db8b0079027185aceedc0a72022d1d
Ismayil338 <150678829+Ismayil-Panahli@users.noreply.github.com>
2024-06-06 15:37:11 +0400

    Add files via upload

---
+++ Install apps using winget.bat
@@ -0,0 +1,1 @@
+powershell -command "winget install 7zip Audacious Hibbiki.Chromium ExplorerPatcher Git.Git TheDocumentFoundation.LibreOffice Localsend Notepad++ qBittorrent.qBittorrent Sumatrapdf Telegram.TelegramDesktop Vscodium Whatsapp"
