aaec8bbc37a74a6367c002e9a79ba4005bd7831d
Ismayil338 <150678829+Ismayil-Panahli@users.noreply.github.com>
2024-06-09 22:47:30 +0400

    Delete Install apps using winget.bat

--- Install apps using winget.bat
+++
@@ -1,1 +0,0 @@
-powershell -command "winget install 7zip Audacious Hibbiki.Chromium ExplorerPatcher Git.Git TheDocumentFoundation.LibreOffice Localsend Notepad++ qBittorrent.qBittorrent Sumatrapdf Telegram.TelegramDesktop Vscodium Whatsapp"
