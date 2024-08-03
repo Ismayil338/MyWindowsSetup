32585677ae7d44cc8d0320faf8ab212566c9c97a
Ismayil <150678829+Ismayil338@users.noreply.github.com>
2024-08-03 23:33:36 +0400

    Add files via upload

---
+++ (Admin) Apps for Windows 10, 11 (choco).bat
@@ -0,0 +1,3 @@
+choco install -y Vcredist140 7zip Audacious Localsend.install Notepadplusplus Powershell-core Sumatrapdf Telegram Transmission
+choco install -y Git --params "/NoShellIntegration"
+choco install -y Vscodium --params "/NoAddContextMenuFiles /NoAddContextMenuFolders /NoDesktopIcon"
