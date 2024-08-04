d51ffb1f3889ad24cce28b06425e629804401691
Ismayil <150678829+Ismayil338@users.noreply.github.com>
2024-08-04 11:18:56 +0400

    Delete (Admin) Apps for Windows 10, 11 (choco).bat

--- (Admin) Apps for Windows 10, 11 (choco).bat
+++
@@ -1,3 +0,0 @@
-choco install -y Vcredist140 7zip Audacious Localsend.install Notepadplusplus Powershell-core Sumatrapdf Telegram Transmission
-choco install -y Git --params "/NoShellIntegration"
-choco install -y Vscodium --params "/NoAddContextMenuFiles /NoAddContextMenuFolders /NoDesktopIcon"
