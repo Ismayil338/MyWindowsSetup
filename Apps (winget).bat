ed7437d2dec14a7063a8c3d6f283fea2acad0ff4
Ismayil <150678829+Ismayil338@users.noreply.github.com>
2024-08-04 11:19:30 +0400

    Add files via upload

--- Apps (winget).bat
+++ Apps (winget).bat
@@ -1,1 +1,1 @@
-winget install 7zip Audacious ExplorerPatcher Git.Git Localsend Notepad++ Outlook Sumatrapdf Telegram.TelegramDesktop Transmission.Transmission Vscodium Whatsapp
+winget install 7zip Audacious Git.Git Localsend Notepad++ Outlook Microsoft.Powershell Sumatrapdf Telegram.TelegramDesktop Transmission.Transmission Vscodium Whatsapp
