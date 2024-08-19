e9d0a5a5c713b76e4da44b973f9d749b2ed826cd
Ismayil <150678829+Ismayil338@users.noreply.github.com>
2024-08-19 17:17:39 +0400

    Add files via upload

--- Win8/Current User settings.bat
+++ Win8/Current User settings.bat
@@ -7,12 +7,16 @@ reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer" /v ShellState 
 :: Turn On Use Check Boxes to Select Items
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v AutoCheckSelect /t REG_DWORD /d 1 /f
 
+:: Open Control Panel with Large icons by default
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\ControlPanel" /v AllItemsIconView /t REG_DWORD /d 0 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\ControlPanel" /v StartupPage /t REG_DWORD /d 1 /f
+
 :: Add Control Panel Desktop Icon
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{5399E694-6CE5-4D6C-8FCE-1D8870FDCBA0}" /t REG_DWORD /d "0" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v {5399E694-6CE5-4D6C-8FCE-1D8870FDCBA0} /t REG_DWORD /d 0 /f
 
 :: Disable Only Upper-right Corner for Charms Hint
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\ImmersiveShell\EdgeUI" /v "DisableTRCorner" /t REG_DWORD /d "1" /f
-reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\ImmersiveShell\EdgeUI" /v "DisableCharmsHint" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\ImmersiveShell\EdgeUI" /v DisableTRCorner /t REG_DWORD /d 1 /f
+reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\ImmersiveShell\EdgeUI" /v DisableCharmsHint /f
 
 taskkill /f /im explorer.exe
 start explorer.exe
