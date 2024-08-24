ff22ca37b17acd6a17461031a72d217e70ab4a7b
Ismayil <150678829+Ismayil338@users.noreply.github.com>
2024-08-24 13:34:32 +0400

    Delete Apps (winget).bat

--- Apps (winget).bat
+++
@@ -1,2 +0,0 @@
-winget install 7zip Audacious Localsend Notepad++ Outlook Microsoft.Powershell Sumatrapdf Telegram.TelegramDesktop Transmission.Transmission Vscodium Whatsapp
-winget install --id=Git.Git --custom '/components="gitlfs,assoc,autoupdate"'
