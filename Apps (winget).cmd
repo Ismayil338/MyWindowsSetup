cec3b984690a3278c6e2c159a830ca0efbc70d23
Ismayil <150678829+Ismayil338@users.noreply.github.com>
2024-09-19 09:11:12 +0400

    Add files via upload

--- Apps (winget).cmd
+++ Apps (winget).cmd
@@ -1,2 +1,2 @@
-winget install 7zip Audacious Hibbiki.Chromium Localsend Notepad++ Outlook Microsoft.Powershell Sumatrapdf Telegram.TelegramDesktop Transmission.Transmission Vscodium Whatsapp
+winget install 7zip Audacious Hibbiki.Chromium Localsend Notepad++ Outlook Microsoft.Powershell Python.Python.3.12 Sumatrapdf Telegram.TelegramDesktop Transmission.Transmission Vscodium Whatsapp
 winget install --id=Git.Git --custom '/components="gitlfs,assoc,autoupdate"'
