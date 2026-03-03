0cca7fe15df91ab9d4ca8debfd5ee55cb7aa1ece
Ismayil <150678829+Ismayil338@users.noreply.github.com>
2026-03-03 22:33:20 +0400

    Remove 'Audacious' from winget install command

--- Apps (winget).cmd
+++ Apps (winget).cmd
@@ -1,3 +1,4 @@
-winget install 7zip Audacious Hibbiki.Chromium Localsend Notepad++ Outlook Microsoft.Powershell Python.Python.3.12 Sumatrapdf Telegram.TelegramDesktop Transmission.Transmission Vscode
+winget install 7zip Hibbiki.Chromium Localsend Notepad++ Outlook Microsoft.Powershell Python.Python.3.12 Sumatrapdf Telegram.TelegramDesktop Transmission.Transmission Vscode
 winget install --id=Git.Git --custom '/components="gitlfs,assoc,autoupdate"'
+
 
