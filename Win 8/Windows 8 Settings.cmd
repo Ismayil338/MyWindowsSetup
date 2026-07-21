3268ccbac0ff2de26eb347c8ad2ac7d85dd26e76
Ismayil <150678829+Ismayil338@users.noreply.github.com>
2026-07-21 23:36:29 +0400

    Update Windows 8 Settings.cmd

--- Win 8/Windows 8 Settings.cmd
+++ Win 8/Windows 8 Settings.cmd
@@ -22,7 +22,6 @@ reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\ImmersiveShell\EdgeUI
 
 :: Disable UAC
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v PromptOnSecureDesktop /t REG_DWORD /d 0 /f
-reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v EnableLUA /t REG_DWORD /d 1 /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v ConsentPromptBehaviorAdmin /t REG_DWORD /d 0 /f
 
 taskkill /f /im explorer.exe
