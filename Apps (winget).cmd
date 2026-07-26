b3bc65f7a4821e757a5a836e484e849628778a97
Ismayil <150678829+Ismayil338@users.noreply.github.com>
2026-07-26 12:03:25 +0400

    Delete Apps (winget).cmd

--- Apps (winget).cmd
+++
@@ -1,4 +0,0 @@
-winget install 7zip Hibbiki.Chromium Localsend Notepad++ Outlook Microsoft.Powershell Python.Python.3.12 Sumatrapdf Telegram.TelegramDesktop Transmission.Transmission Vscode
-winget install --id=Git.Git --custom '/components="gitlfs,assoc,autoupdate"'
-
-
